ef0cdb530c3f7ce57d779dc6c1b5013d9fa07f1d
Lisa Dean <lisa@lisadean.net>
2019-04-28 17:44:29 -0400

    Problem 5 done with help. Learned about lambdas in python

--- problem5.py
+++ problem5.py
@@ -1,3 +1,4 @@
+# MEDIUM
 # cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first
 # and last element of that pair. For example, car(cons(3, 4)) returns 3, and
 # cdr(cons(3, 4)) returns 4.
@@ -9,3 +10,21 @@
 #         return f(a, b)
 #     return pair
 # Implement car and cdr.
+
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+
+def car(pair):
+    return pair(lambda a, b: a)
+
+
+def cdr(pair):
+    return pair(lambda a, b: b)
+
+
+assert car(cons(3, 4)) == 3
+assert cdr(cons(3, 4)) == 4
